ca12483db8daaf069fa570dd202b89c99877283c
lillian-jewel <lillian.j.arcos@gmail.com>
2023-03-24 15:17:22 -0500

    Syntax, Types, & Variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,79 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+//        System.out.println("Good to see you");
+//
+//        /* one liner */
+//        /**
+//         * multi line
+//         * ln
+//         * ln
+//         */
+//
+//        // numbers do we have in js
+//
+////        byte myByte = 127;
+////        myByte = myByte+1;
+////        System.out.println(myByte);
+//
+//        // strings and chars
+//        System.out.println();
+//        System.out.println("Double quotes here works just fine for a String");
+//        System.out.println("A");
+//
+//        // escape characters
+//        System.out.print("\"He said this thing\" \n");
+//        System.out.print("\t \\ Slashes \\ \n");
+//
+//    // how to work initializing / declaring variables
+//        //initialize
+//        String myName;
+//        int myAge;
+//
+//        //then declare
+//        myName = "Lilly";
+//        myAge = 21;
+//
+//        //initialize & declare
+//        String palindrome = "Cool";
+//
+//        // what does a constant look like in Java?
+//        final String myCity = "San Antonio";
+//
+//        System.out.println("Hey im from " + myCity);
+
+///////////
+        int myFavoriteNumber = 40;
+        System.out.println(myFavoriteNumber);
+
+
+        String myString;
+        myString = "Lilly";
+        System.out.println(myString);
+
+        //
+
+//        float myNumber;
+//        myNumber = ;
+//        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String class;
+
+
+
+
+
+
+
+    }
+}
+
+
