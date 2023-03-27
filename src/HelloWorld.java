7452c74dab86b66e5b6043900d103babbbab74fe
lillian-jewel <lillian.j.arcos@gmail.com>
2023-03-26 23:21:08 -0500

    Syntax, Types, Variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -57,19 +57,36 @@ public class HelloWorld {
 //        myNumber = ;
 //        System.out.println(myNumber);
 
-        int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
-//        String class;
+//        int class;
 
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
 
+//examples
+//        myNumber += 9;
+//// equivalent to
+//        myNumber = myNumber + 9;
 
+//        int x = 4;
+//        x += 5;
 
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
 
 
 
